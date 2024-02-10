66110377ca3702b9fb7b79508e41905557e1934c
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-02-10 21:01:55 +0600

    footer dynamic updated date feature implmented

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 
 
 const Footer = () => {
+    const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+          setCurrentYear(new Date().getFullYear());
+        }, 60000); 
+    
+        return () => clearInterval(intervalId); 
+      }, []);
     return (
         <div className="max-w-6xl mx-auto">
                 <footer className="footer footer-center p-4 bg-neutral-200 text-base-content">
         <aside>
-            <p className="text-lg font-medium">Copyright © 2023 - All right reserved by MD Abdur Rahman</p>
+            <p className="text-lg font-medium">Copyright © {currentYear} - All right reserved by MD Abdur Rahman</p>
         </aside>
         </footer> 
         </div>
